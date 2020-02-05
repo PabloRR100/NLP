741e98b2c0bc6cace030a78e689aa4f33741e205
PabloRR10 <pabloruizruiz10@gmail.com>
2020-02-05 12:07:26 +0100

    structure for everything inside scripts

---
+++ Side_projects/Document_Clustering/scripts/algorithms/clustering/cluster.py
@@ -0,0 +1,130 @@
+
+'''
+
+Run all the clustering in a single runs
+=======================================
+
+1 - Load the Catalog created in main.py
+2 - Apply different clustering algorithsm
+3 - Store results for frontend visualization
+
+'''
+
+import os
+import sys
+import pickle
+from os.path import join as JP
+
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+from collections import defaultdict
+
+import spacy
+import sklearn
+from spacy import displacy
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+DOCKER = os.environ.get('IM_IN_DOCKER', False)
+if DOCKER: 
+    WORKDIR = '/app/'
+else:
+    WORKDIR = 'c:\\Users\\RUIZP4\\Documents\\DOCS\\Pablo_Personal\\StanfordNLP\\Side_projects\\Document_Clustering'
+    WORKDIR = '/Users/pabloruizruiz/OneDrive/Courses/NLP_Stanford/Side_projects/Document_Clustering/'
+
+os.chdir(WORKDIR)
+print('WORKING DIRECTORY: ', WORKDIR)
+sys.path.append(WORKDIR)
+sys.path.append(JP(WORKDIR,'utils'))
+sys.path.append(JP(WORKDIR,'scripts'))
+from utils.general import parse_yaml
+
+config = parse_yaml('config.yaml')
+paths = config['paths']
+
+
+''' 1 - Data ''' 
+
+catalog = load_catalog(paths['catalog'], name='bbc_news')
+tfidf = catalog.models['TFIDF']
+data = tfidf.representation
+# data = tfidf.representation.values
+
+
+''' 2 - Clustering '''
+
+MIN_K = 2
+MAX_K = 10
+
+EMBED_SIZE = 50
+UMAP_EPOCHS = 50
+MIN_CLUSTER_SIZE = 50
+
+words_results = defaultdict(lambda: defaultdict())
+embedding_results = defaultdict(lambda: defaultdict())
+nested_dict = lambda: defaultdict(nested_dict)
+cluster_results = nested_dict()
+
+
+import umap
+# import hdbscan
+from sklearn.decomposition import PCA
+from scripts.algorithms.clustering import kmeans_clustering, kmedoids_clustering
+from scripts.algorithms.clustering import plot_centroids_as_wordclouds
+
+
+dim_reducters = [
+    PCA(n_components=EMBED_SIZE, random_state=46),
+    umap.UMAP(
+        n_epochs=UMAP_EPOCHS, n_neighbors=5, 
+        n_components=3, metric='cosine', 
+        random_state=46, verbose=True)]
+
+clustering_methods = [
+    kmeans_clustering, 
+    kmedoids_clustering]
+    # hdbscan.HDBSCAN(min_samples=10, min_cluster_size=MIN_CLUSTER_SIZE)]
+
+def cluster(dim_reducters, MIN_K, MAX_K, ):
+
+    for reducter in tqdm(dim_reducters):
+        
+        data_low_dim = reducter.fit_transform(data.copy())
+        
+        for k in tqdm(range(MIN_K, MAX_K)):
+            
+            for method in clustering_methods:
+                
+                print('[INFO]: Num_clusters = {} Method = {} Red_dim_technique = {}'.format(
+                    k, method.__name__, reducter.__class__.__name__))
+
+                # Run algorithm
+                clusters = method(data_low_dim.copy(), num_clusters=k)
+                # words = words_per_cluster(tfidf, clusters)
+
+                # Compute word importance -> centroids
+                data['cluster'] = clusters.labels_
+                centroids = data.groupby('cluster').mean().reset_index()
+                scores = pd.melt(centroids, id_vars=['cluster'], var_name='word', value_name='score')
+                
+                # Collect data_low_dim with labes
+                data_low_dim_df = pd.DataFrame(data_low_dim, columns=['d{}'.format(i+1) for i in range(data_low_dim.shape[1])])
+                data_low_dim_df['cluster'] = clusters.labels_
+                
+                # Store clustering results
+                cluster_results[k][method.__name__][reducter.__class__.__name__]['scatter'] = data_low_dim_df
+                cluster_results[k][method.__name__][reducter.__class__.__name__]['wordclouds'] = scores
+
+                # Create and store wordclouds
+                # fig = plot_centroids_as_wordclouds(scores, n_cols=1, figsize=(15,15))
+                # fig = plot_centroids_as_wordclouds(scores, n_cols=2-k%2)
+                # fig.savefig(JP(paths['images'], 'wordcloud_clusters_{}_{}_{}.png'.format(k, method.__name__, reducter.__class__.__name__)))
+
+    # SAVE
+    print('[INFO]: Saving ...')
+    with open(JP(paths['results'], 'clustering.pkl'), 'wb') as f:
+        pickle.dump(defaultdict_to_dict(cluster_results), f)
+
+exit()
