56b44cefd1a65d51e5fbaae5cef6890167f7d3aa
PabloRR100 <pabloruizruiz10@gmail.com>
2020-02-08 13:13:37 +0100

    model loading and model inference

--- Assignments/Assig_4_NMT/app/inference.py
+++ Assignments/Assig_4_NMT/app/inference.py
@@ -1,5 +1,34 @@
 
 import torch
 import torch.nn as nn
+from nmt_model import NMT 
 
-model = torch.
+# x = input('Español: ', )
+x = 'Me gustan los libros'
+
+# Load Model
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+model = torch.load('model.bin', map_location=torch.device(device))
+print('Inside the model: [{}]'.format(model.keys()))
+
+net = NMT(
+    embed_size=model['args']['embed_size'],
+    hidden_size=model['args']['hidden_size'],
+    vocab=model['vocab'],
+    dropout_rate=model['args']['dropout_rate'])
+
+net.load_state_dict(model['state_dict'])
+net.eval()
+
+x = x.split(' ')
+net.beam_search(x)[0].value
+
+# Direct Approach Using the Class
+net2 = NMT.load('model.bin')
+net2.eval()
+net2.beam_search(x)[0].value
+
+
+
+
+
