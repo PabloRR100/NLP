b3bb0c6091d5bd603d0c591330054204cc299071
PabloRR100 <pabloruizruiz10@gmail.com>
2020-02-20 00:34:11 +0100

    testing entirely the enconder

--- Assignments/Assig_5_NMT_CNN_CharLevel/app/nmt_model.py
+++ Assignments/Assig_5_NMT_CNN_CharLevel/app/nmt_model.py
@@ -71,7 +71,13 @@ class NMT(nn.Module):
         source_lengths = [len(s) for s in source]
 
         # Convert list of lists into tensors
+        target_padded = self.vocab.tgt.to_input_tensor(target, device=self.device)
+        source_padded_chars = self.vocab.src.to_input_tensor_char(source, device=self.device)
+        target_padded_chars = self.vocab.tgt.to_input_tensor(target, device=self.device)
 
+        enc_hiddens, dec_init_state = self.encode(source_padded_chars, source_lengths)
+        enc_masks = self.generate_sent_masks(enc_hiddens, source_lengths)
+        combined_outputs = self.decode(enc_hiddens, enc_masks, dec_init_state, target_padded)
         ## A4 code
         # source_padded = self.vocab.src.to_input_tensor(source, device=self.device)   # Tensor: (src_len, b)
         # target_padded = self.vocab.tgt.to_input_tensor(target, device=self.device)   # Tensor: (tgt_len, b)
@@ -90,7 +96,6 @@ class NMT(nn.Module):
         ###     - Add `target_padded_chars` for character level padded encodings for target
         ###     - Modify calls to encode() and decode() to use the character level encodings
 
- 
         ### END YOUR CODE
 
         P = F.log_softmax(self.target_vocab_projection(combined_outputs), dim=-1)
